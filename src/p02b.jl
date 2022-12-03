f7e3ba69730d4810db98808cff59e8933be39dfa
Dan Huang <dan@northbeam.io>
2022-12-02 22:58:58 -0800

    correct p02b

--- src/p02b.jl
+++ src/p02b.jl
@@ -1,24 +1,34 @@
-OUTCOMES = [
-    3 6 0
-    0 3 6
-    6 0 3
-]
-
 PLAY_TO_INDEX = Dict(:rock => 1, :paper => 2, :scizzors => 3)
 INDEX_TO_PLAY = [:rock :paper :scizzors]
-OUTCOME_TO_DIRECTION = Dict(:win => 1, :draw => 0, :lose => -1)
 SHAPE_SCORE = Dict(:rock => 1, :paper => 2, :scizzors => 3)
+OUTCOME_SCORE = Dict(:win => 6, :draw => 3, :lose => 0)
 
-function outcome_score(opponent_play, player_play)
-    return OUTCOMES[PLAY_TO_INDEX[opponent_play], PLAY_TO_INDEX[player_play]]
+function outcome(opponent_play, player_play)
+    result_indicator = (PLAY_TO_INDEX[player_play] - PLAY_TO_INDEX[opponent_play] + 3) % 3
+    if result_indicator == 0
+        return :draw
+    elseif result_indicator == 1
+        return :win
+    else
+        return :lose
+    end
 end
 
-function shape_score(opponent_play, player_play)
-    return SHAPE_SCORE[player_play]
+function deduce_play(opponent_play, player_outcome)
+    for player_play in INDEX_TO_PLAY
+        if outcome(opponent_play, player_play) == player_outcome
+            return player_play
+        end
+    end
+    throw(InvalidStateException("Could not find correct play for player outcome"))
 end
+
+@assert outcome(:rock, :paper) == :win
+@assert outcome(:rock, :rock) == :draw
+@assert outcome(:rock, :scizzors) == :lose
 
 OPPONENT_MAPPING = Dict("A" => :rock, "B" => :paper, "C" => :scizzors)
-SELF_MAPPING = Dict("X" => :win, "Y" => :draw, "Z" => :lose)
+SELF_MAPPING = Dict("X" => :lose, "Y" => :draw, "Z" => :win)
 
 function main(filename)
     filetext = open(filename) do f
@@ -29,10 +39,9 @@ function main(filename)
 
     opponent_plays = map((x) -> OPPONENT_MAPPING[x], filetokens[1:2:end])
     self_outcomes = map((x) -> SELF_MAPPING[x], filetokens[2:2:end])
-    self_plays = map((opponent_play, self_outcome) -> INDEX_TO_PLAY[(OUTCOME_TO_DIRECTION[self_outcome] + PLAY_TO_INDEX[opponent_play]) % 3 + 1], opponent_plays, self_outcomes)
-
-    outcome_scores = map(outcome_score, opponent_plays, self_plays)
-    shape_scores = map(shape_score, opponent_plays, self_plays)
+    self_plays = map(deduce_play, opponent_plays, self_outcomes)
+    outcome_scores = map((x) -> OUTCOME_SCORE[x], self_outcomes)
+    shape_scores = map((x) -> SHAPE_SCORE[x], self_plays)
 
     println(sum(outcome_scores + shape_scores))
 end
